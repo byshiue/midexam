3a18b5ace59581c306147be8dcf3634856ef90f7
C14016112 <byshiue@gmail.com>
2015-04-16 23:19:50 +0800

    The basic function of heapsort.

---
+++ heapsort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int HEAPIFY( int A[], int posi) // posi :position
+{
+	
+}
+
+int PARENT( int x) 
+{
+	return (x-1) / 2 ; 
+}
+
+int LEFT(int x)
+{
+	return (2*x+1) ; 
+}
+
+int RIGHT( int x) 
+{
+	return( 2*x+2) ; 
+}
