f7663ba8b5ca9382bab582db4baa1d7e2b6e5c95
C14016112 <byshiue@gmail.com>
2015-04-29 10:46:50 +0800

    complete

--- fft_quick.cpp
+++ fft_quick.cpp
@@ -6,16 +6,19 @@
 #include<time.h>
 using namespace std;
 
+int FFT( double* x_r, double* x_i, double* y_r, double* y_i, int N ) ; 
 int main()
 {
 	// y_k = sum( x_n * w^{-kn}, n=0..N-1}
 	// w = cos(2*pi / N) + i*sin(2*pi/N)
+	
 	double *y_r, *y_i, *x_r, *x_i, w_r, w_i, t1, t2 ; 
 									// x_r : real part of x 
 									// x_i : image part of x 
 									// y_r : real part of y
 									// y_i : image part of y
 	int k, n, N ;  // in put an integer to N 
+	cout << "Please enter an interger:" ;
 	cin  >> N ; 
 	
 	// create memory of x, y 
@@ -32,7 +35,7 @@ int main()
 	
 	FFT( x_r, x_i, y_r, y_i, N ) ; 
 	
-/*	
+
 	t1 = clock() ; 
 	for ( int k = 0 ; k < N ; k++)
 	{
@@ -54,21 +57,27 @@ int main()
 	for ( int i = 0 ; i< N ; i++) 
 	{
 		cout << setw(6) ; 
-		cout << "第" << i << "個y的實部為:" << y_r[i] << " "
-			 << "第" << i << "個y的虛部為:" << y_i[i] << " " << endl ;
+		cout << "第" << i << "個y的實部為:" << y_r[i] << "  "
+			 << "第" << i << "個y的虛部為:" << y_i[i] << "i" << " " << endl ;
 			 
 	}
 	cout << (t2-t1)/1000 << endl ;
 	
 	return 0 ;
-*/	
+	
 }
 
 
 // yk = sum ( x(n) * w^(-kn) , n=0..N-1)
 // yk = sum ( x(n) 
-int FFT( double* x_r, double* x_i, double* y_r, double* y_i, int N ) ; 
+int FFT( double* x_r, double* x_i, double* y_r, double* y_i, int N )  
 {
+	if ( N==1)
+	{
+		y_r[0] = x_r[0] ; 
+		y_i[0] = x_i[0] ; 
+		return 0 ;
+	}
 	int k, n ;
 	double *u_r, *u_i, *v_r, *v_i , w_r, w_i ; 
 	
@@ -86,5 +95,18 @@ int FFT( double* x_r, double* x_i, double* y_r, double* y_i, int N ) ;
 	}
 	FFT(u_r, u_i, v_r, v_i, N/2 ) ; 
 	FFT(u_r+N/2, u_i+N/2, v_r+N/2, v_i+N/2, N/2 ) ; 
+	
+	// compute y 
+	for ( k = 0 ; k<N/2; k++) 
+	{
+		// w^{-k} 
+		w_r = cos(-k*2*M_PI/N) ; 
+		w_i = sin(-k*2*M_PI/N) ; 
+		y_r[k] = u_r[k] + w_r*u_r[k+N/2] - w_i*u_i[k+N/2] ;
+		y_i[k] = u_i[k] + w_r*u_i[k+N/2] + w_i*u_r[k+N/2] ;
+		y_r[k+N/2] = u_r[k] - (w_r*u_r[k+N/2] - w_i*u_i[k+N/2] ) ;
+		y_i[k+N/2] = u_i[k] - (w_r*u_i[k+N/2] + w_i*u_r[k+N/2] ) ; 
+	}
+	
 }
 
