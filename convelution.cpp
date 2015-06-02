118092c065021466ae504d3e52ba27ed539dd385
C14016112 <byshiue@gmail.com>
2015-06-03 07:47:28 +0800

    改成SIZE一定會在2的K次方

--- convelution.cpp
+++ convelution.cpp
@@ -3,28 +3,31 @@
 using namespace std;
 
 
-int * poly ( int value)
+int * poly ( int value, int & len)
 {
 	int N = 1+ int( log10(value) ), M = 2*N-1;
+	int size = 1 ; 
+	while ( size < M) size *= 2  ;
 	int *pv ; 
-	pv = new int[M] ; 
-	for ( int i = 0 ; i< M; i++) pv[i] = 0 ; 
+	pv = new int[size] ; 
+	for ( int i = 0 ; i< size; i++) pv[i] = 0 ; 
 	int k = N-1 ; 
 	while ( value > 0 )
 	{
-		pv[M-1-k] = int ( value/ (int(pow(10,k)) )) ; 
-		value -= pv[M-1-k]*pow(10,k) ; 
+		pv[size-1-k] = int ( value/ (int(pow(10,k)) )) ; 
+		value -= pv[size-1-k]*pow(10,k) ; 
 		k-- ;
 	}
+	len = size;
 	return pv ; 
 }
 
 int main()
 {
 	int * p ; 
-	int n ; 
+	int n,len ; 
 	cin >> n ; 
-	p = poly(n)  ; 
-	for ( int i = 0 ; i< 1+2*int(log10(n) );i++ ) cout << p[i] << " " ;
+	p = poly(n,len)  ; 
+	for ( int i = 0 ; i< len;i++ ) cout << p[i] << " " ;
 	cout << endl ;
 }
